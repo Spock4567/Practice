3f87bd04bf6ad60bc221e4380d8c9fb7b8ffa0e2
korol <korol@LAPTOP-PRMP91HU>
2025-04-14 20:00:01 +0300

    Добавьте файлы проекта.

---
+++ Practice/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        
+        //проверка строки на чётность
+        static bool IsEven(string str)
+        {
+            int length = str.Length;
+            if (length % 2 == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        //функция для обработки строки
+        //проверяем четная ли строка
+        //в зависимости от её четности/нечетности вызываем ту или иную функцию
+        static void StringProcessing(string str, out string newStr)
+        {
+            if (IsEven(str))
+                newStr = EvenStringProcessing(str);
+            else
+                newStr = OddStringProcessing(str);
+        }
+
+        // обработка строк с четным количеством сммволов
+        static string EvenStringProcessing(string str)
+        {
+            int halfLength = str.Length / 2;
+            string firstHalf = str.Substring(0, halfLength);
+            string secondHalf = str.Substring(halfLength);
+            firstHalf = ReverseString(firstHalf);
+            secondHalf = ReverseString(secondHalf);
+            string newStr = string.Concat(firstHalf, secondHalf);
+            return newStr;
+        }
+
+        // обработка строк с нечетным количеством сммволов  
+        static string OddStringProcessing(string str)
+        {
+            string reversedStr = ReverseString(str);
+            string newStr = string.Concat(reversedStr, str);
+            return newStr;
+        }
+
+        //переворот строки
+        static string ReverseString(string str)
+        {
+            string newStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                newStr += str[i];
+            }
+            return newStr;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            StringProcessing(str, out string newStr);
+            Console.WriteLine("Обработанная строка:");
+            if (newStr != string.Empty)
+            {
+                Console.WriteLine(newStr);
+            }
+        }
+    }
+}
